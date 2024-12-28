4c36915791d5a7d866e5bfefcd7dbc2a6f4040db
horiaomar25 <horiaomar6@gmail.com>
2024-12-28 16:49:17 +0000

    Spring Boot application added

---
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/SchoolStaffRecrutimentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchoolStaffRecrutimentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolStaffRecrutimentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolStaffRecrutimentPlatformApplication.class, args);
+	}
+
+}
