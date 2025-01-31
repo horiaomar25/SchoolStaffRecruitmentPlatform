a043727f2d2126ec5c5d3e858dbe56f9a1a2a4e6
horiaomar25 <horiaomar6@gmail.com>
2025-01-31 17:34:34 +0000

    added: update on availability controller, will keep working on it

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AvailabilityController.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AvailabilityController.java
@@ -1,11 +1,14 @@
 package com.example.SchoolStaffRecrutimentPlatform.controller;
 
+import com.example.SchoolStaffRecrutimentPlatform.entities.AppUser;
 import com.example.SchoolStaffRecrutimentPlatform.entities.Availability;
+import com.example.SchoolStaffRecrutimentPlatform.repository.AppUserRepository;
 import com.example.SchoolStaffRecrutimentPlatform.repository.AvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/availability")
@@ -13,6 +16,8 @@ public class AvailabilityController {
 
     @Autowired
     private AvailabilityRepository availabilityRepository;
+    @Autowired
+    private AppUserRepository appUserRepository;
 
     @GetMapping("/getdates")
     public List<Availability> getAllAvailability() {
@@ -21,8 +26,14 @@ public class AvailabilityController {
 
     @PostMapping("/create")
     public Availability createAvailability(@RequestBody Availability availability) {
-        Availability savedAvailability = availabilityRepository.save(availability);
-        return availabilityRepository.save(availability);
+        Optional<AppUser> user = appUserRepository.findById(availability.getUser().getId());
+
+        Availability savedAvailability = new Availability();
+        savedAvailability.setStartDate(availability.getStartDate());
+        savedAvailability.setEndDate(availability.getEndDate());
+        savedAvailability.setUser(user.get());
+
+        return availabilityRepository.save(savedAvailability);
     }
 
 }
