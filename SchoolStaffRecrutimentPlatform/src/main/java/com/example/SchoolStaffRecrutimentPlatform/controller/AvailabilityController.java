e443da4509b64698bc4e780dda31b501848fcad8
horiaomar25 <horiaomar6@gmail.com>
2025-01-31 16:47:58 +0000

    added: repository file and controller

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AvailabilityController.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AvailabilityController.java
@@ -1,4 +1,27 @@
 package com.example.SchoolStaffRecrutimentPlatform.controller;
 
+import com.example.SchoolStaffRecrutimentPlatform.entities.Availability;
+import com.example.SchoolStaffRecrutimentPlatform.repository.AvailabilityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/availability")
 public class AvailabilityController {
+
+    @Autowired
+    private AvailabilityRepository availabilityRepository;
+
+    @GetMapping
+    public List<Availability> getAllAvailability() {
+        return availabilityRepository.findAll();
+    }
+
+    @PostMapping
+    public Availability createAvailability(@RequestBody Availability availability) {
+        return availabilityRepository.save(availability);
+    }
+
 }
