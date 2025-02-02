2c5c4db5468478d5e694eae147c06a01f5c84cc1
horiaomar25 <horiaomar6@gmail.com>
2025-02-02 16:20:57 +0000

    added: update to Assignment Controller

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AssignmentController.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AssignmentController.java
@@ -1,4 +1,37 @@
 package com.example.SchoolStaffRecrutimentPlatform.controller;
 
+import com.example.SchoolStaffRecrutimentPlatform.entities.AppUser;
+import com.example.SchoolStaffRecrutimentPlatform.entities.Assignment;
+import com.example.SchoolStaffRecrutimentPlatform.repository.AppUserRepository;
+import com.example.SchoolStaffRecrutimentPlatform.repository.AssignmentRepository;
+import com.example.SchoolStaffRecrutimentPlatform.repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/assignments")
 public class AssignmentController {
+
+    @Autowired
+    SchoolRepository schoolRepository;
+
+    @Autowired
+    AppUserRepository appUserRepository;
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+
+    // Get all unassigned
+    @GetMapping("/unassigned")
+    public ResponseEntity<List<Assignment>> getAllAssignments() {
+        List<Assignment> unassignedAssignment = assignmentRepository.findByUserIsNull();
+        if (unassignedAssignment.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(unassignedAssignment);
+    }
 }
--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/repository/AssignmentRepository.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/repository/AssignmentRepository.java
@@ -4,4 +4,11 @@ import com.example.SchoolStaffRecrutimentPlatform.entities.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
+@Repository
+public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
+    List<Assignment> findByUserId(int userId);
+
+    List<Assignment> findByUserIsNull();
+}
