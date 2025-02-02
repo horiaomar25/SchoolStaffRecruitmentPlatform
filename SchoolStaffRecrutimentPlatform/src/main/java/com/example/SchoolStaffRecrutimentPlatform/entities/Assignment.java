5e4ce7f6babe780e99b9efcee5c86eba2352b01a
horiaomar25 <horiaomar6@gmail.com>
2025-02-02 14:10:01 +0000

    added: assignment entity created

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/entities/Assignment.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/entities/Assignment.java
@@ -1,4 +1,44 @@
 package com.example.SchoolStaffRecrutimentPlatform.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+@Getter
+@Setter
+@Entity
+@Table
 public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String position;
+
+    @Column(columnDefinition = "TEXT")
+    private String assignmentDescription;
+
+
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id", nullable = false)
+    private School school;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = true) // initially null so that when a user accepts the assignment it will be added to their dashboard
+    private AppUser user;
+
+
+
+
+
+
 }
