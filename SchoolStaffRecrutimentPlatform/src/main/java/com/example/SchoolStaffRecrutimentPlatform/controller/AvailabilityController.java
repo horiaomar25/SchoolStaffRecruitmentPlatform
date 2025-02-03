facfaaeebf0ac0627c095996b238a59fe092a0dc
horiaomar25 <horiaomar6@gmail.com>
2025-02-03 16:38:36 +0000

    added: will start working on AssignmentDTO

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AvailabilityController.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/controller/AvailabilityController.java
@@ -1,9 +1,6 @@
 package com.example.SchoolStaffRecrutimentPlatform.controller;
 
-import com.example.SchoolStaffRecrutimentPlatform.dto.AppUserDTO;
-import com.example.SchoolStaffRecrutimentPlatform.dto.AvailabilityDTO;
-import com.example.SchoolStaffRecrutimentPlatform.entities.AppUser;
-import com.example.SchoolStaffRecrutimentPlatform.entities.Availability;
+
 import com.example.SchoolStaffRecrutimentPlatform.repository.AppUserRepository;
 import com.example.SchoolStaffRecrutimentPlatform.repository.AvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,36 +21,7 @@ public class AvailabilityController {
     @Autowired
     private AppUserRepository appUserRepository;
 
-    @GetMapping("/getdates")
-    public List<AvailabilityDTO> getAllAvailability() {
-        List<Availability> availabilities = availabilityRepository.findAll();
 
-        // Convert the list of Availability to AvailabilityDTO
-        List<AvailabilityDTO> availabilityDTOs = new ArrayList<>();
-        for (Availability availability : availabilities) {
-            AppUserDTO userDTO = new AppUserDTO(availability.getUser().getId(), availability.getUser().getUsername());
-            AvailabilityDTO availabilityDTO = new AvailabilityDTO(availability.getId(), availability.getStartDate(), availability.getEndDate(), userDTO);
-            availabilityDTOs.add(availabilityDTO);
-        }
-
-        return availabilityDTOs;
-    }
-
-    @PostMapping("/create")
-    public Availability createAvailability(@RequestBody AvailabilityDTO availabilityDTO, Principal principal) {
-        // Get authenticated user
-        String username = principal.getName();
-        AppUser user = appUserRepository.findByUsername(username);
-
-        // Create the Availability entity from the DTO
-        Availability availability = new Availability();
-        availability.setStartDate(availabilityDTO.getStartDate());
-        availability.setEndDate(availabilityDTO.getEndDate());
-        availability.setUser(user);
-
-        // Save to the repository
-        return availabilityRepository.save(availability);
-    }
 
 
 }
