a342d6b0d43e4dbeea7e398af3cd3b3d1d089e81
horiaomar25 <horiaomar6@gmail.com>
2025-02-03 16:19:21 +0000

    added: making sure that all repository class are annonated

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/repository/ProfileRepository.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/repository/ProfileRepository.java
@@ -3,7 +3,9 @@ package com.example.SchoolStaffRecrutimentPlatform.repository;
 import com.example.SchoolStaffRecrutimentPlatform.entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProfileRepository extends CrudRepository<Profile, Integer> {
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Integer> {
 // find the Profile by AppUser id (user_id)
 }
