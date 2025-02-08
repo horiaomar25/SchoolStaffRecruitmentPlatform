12c53a68ff862d18e4e98a451379950f912b1488
horiaomar25 <horiaomar6@gmail.com>
2025-02-08 15:20:24 +0000

    added: timesheetdates entity create to store the dates between the start date and end date of assignment

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/entities/TimeSheetDates.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/entities/TimeSheetDates.java
@@ -13,4 +13,15 @@ import java.time.LocalDate;
 public class TimeSheetDates {
 
     // List the dates between the startDate and endDate from the Assignment Entity to create the timesheet
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalDate date; // holding each date for the timesheet for user to tick off
+
+    @ManyToOne
+    @JoinColumn(name = "timesheet_id", nullable = false) // foriegn key to TimeSheet
+    private TimeSheet timeSheet;
+
+    public TimeSheetDates() {}
 }
