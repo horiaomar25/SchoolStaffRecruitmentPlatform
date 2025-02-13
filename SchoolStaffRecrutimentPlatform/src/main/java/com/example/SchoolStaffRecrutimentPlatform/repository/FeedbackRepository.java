377247088ceffa3c1c61ec7c9d4cdb41b568847a
horiaomar25 <horiaomar6@gmail.com>
2025-02-13 20:43:46 +0000

    updated: test for profile controller. Added comments and tidying up code

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/repository/FeedbackRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.SchoolStaffRecrutimentPlatform.repository;
-
-public interface FeedbackRepository {
-}
--- SchoolStaffRecrutimentPlatform/src/test/java/com/example/SchoolStaffRecrutimentPlatform/controller/AuthControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.SchoolStaffRecrutimentPlatform.controller;
-
-public class AuthControllerTest {
-}
