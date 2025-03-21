311f10301a103bb44b987a07de21029ebb5efd21
horiaomar25 <horiaomar6@gmail.com>
2025-03-21 15:23:49 +0000

    updated: yaml file to create jar file to enable build of dockerfile for deployment

--- SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/SchoolStaffRecrutimentPlatformApplication.java
+++ SchoolStaffRecrutimentPlatform/src/main/java/com/example/SchoolStaffRecrutimentPlatform/SchoolStaffRecrutimentPlatformApplication.java
@@ -1,12 +1,14 @@
 package com.example.SchoolStaffRecrutimentPlatform;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolStaffRecrutimentPlatformApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(SchoolStaffRecrutimentPlatformApplication.class, args);
 	}
 
