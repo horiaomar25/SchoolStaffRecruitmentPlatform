0d6488c4db321d8738777a7f7d74dc10773f9d6e
horiaomar25 <horiaomar6@gmail.com>
2025-03-24 18:02:17 +0000

    Refactored file name due to typo

--- src/main/java/com/example/SchoolStaffRecrutimentPlatform/SchoolStaffRecruitmentPlatformApplication.java
+++ src/main/java/com/example/SchoolStaffRecrutimentPlatform/SchoolStaffRecruitmentPlatformApplication.java
@@ -5,11 +5,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolStaffRecrutimentPlatformApplication {
+public class SchoolStaffRecruitmentPlatformApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		SpringApplication.run(SchoolStaffRecrutimentPlatformApplication.class, args);
+		SpringApplication.run(SchoolStaffRecruitmentPlatformApplication.class, args);
 	}
 
 }
