50ccfd66bc9bfad0b15ad7756ea812442d21d7c9
horiaomar25 <horiaomar6@gmail.com>
2025-03-24 22:35:36 +0000

    updated /validate endpoint

--- src/main/java/com/example/SchoolStaffRecrutimentPlatform/config/JWTConfig.java
+++ src/main/java/com/example/SchoolStaffRecrutimentPlatform/config/JWTConfig.java
@@ -32,7 +32,7 @@ public class JWTConfig {
         http.csrf().disable()
                 .authorizeRequests()
                 .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/logout").permitAll()// these endpoints allow to be accessed without authentication
-                .requestMatchers("/api/v1/profile/delete/{id}","/api/v1/profile/personal",  "/api/v1/profile/update", "/api/v1/assignments/accepted","/api/v1/assignments/{assignmentId}/accept","/api/v1/assignments/{assignmentId}/timesheet","/api/v1/assignments/{assignmentId}/timesheet").authenticated() // cannot delete without authenication
+                .requestMatchers("/api/v1/profile/delete/{id}","/api/v1/profile/personal",  "/api/v1/profile/update", "/api/v1/assignments/accepted","/api/v1/assignments/{assignmentId}/accept","/api/v1/assignments/{assignmentId}/timesheet","/api/v1/assignments/{assignmentId}/timesheet", "/api/v1/auth/validate").authenticated() // cannot delete without authenication
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
