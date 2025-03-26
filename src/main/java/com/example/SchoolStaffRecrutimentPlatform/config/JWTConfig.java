5942db8c20102d8c5ef3ce715da73843318d0f68
horiaomar25 <horiaomar6@gmail.com>
2025-03-26 15:37:29 +0000

    update: reset

--- src/main/java/com/example/SchoolStaffRecrutimentPlatform/config/JWTConfig.java
+++ src/main/java/com/example/SchoolStaffRecrutimentPlatform/config/JWTConfig.java
@@ -1,49 +1,46 @@
 package com.example.SchoolStaffRecrutimentPlatform.config;
 
+import com.example.SchoolStaffRecrutimentPlatform.jwt.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.UserDetailsService; // Import UserDetailsService
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class JWTConfig {
 
+    private JwtFilter jwtFilter;
+
     @Autowired
-    private UserDetailsService userDetailsService; // Inject UserDetailsService
+    public void setJwtFilter(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register").permitAll() // Allow login and register
+                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/logout", "/api/v1/auth/validate").permitAll()
                 .requestMatchers("/api/v1/profile/delete/{id}", "/api/v1/profile/personal", "/api/v1/profile/update", "/api/v1/assignments/accepted", "/api/v1/assignments/{assignmentId}/accept", "/api/v1/assignments/{assignmentId}/timesheet", "/api/v1/assignments/{assignmentId}/timesheet").authenticated()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin(withDefaults()) // Enable form-based login
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .cors()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED); // Use sessions
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
