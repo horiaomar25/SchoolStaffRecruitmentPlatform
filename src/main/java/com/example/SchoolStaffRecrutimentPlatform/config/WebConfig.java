d00636d8c8d40380ec40dbb6b93c2e82992a171c
horiaomar25 <horiaomar6@gmail.com>
2025-03-29 16:30:12 +0000

    reset for WebConfig

--- src/main/java/com/example/SchoolStaffRecrutimentPlatform/config/WebConfig.java
+++ src/main/java/com/example/SchoolStaffRecrutimentPlatform/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("https://srs-nu.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
